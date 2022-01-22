02384d70d387da5b8e261ce2f530abdf0ef83209
Devesh Anand Srivastava <contactdeveshanand@gmail.com>
2022-01-22 18:45:54 +0530

    trying things out

--- src/App.js
+++ src/App.js
@@ -5,26 +5,53 @@ import News from './components/News';
 
 function App() {
   const [stories, setStories] = useState([]);
+  const [news, setNews] = useState([]);
+
+  const getId = async () => {
+    const response = await fetch("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
+    const ids = await response.json();
+    setStories(ids);
+    console.log(ids);
+  };//try using promise
+  async function getStory(id) {
+    const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`);
+    const data = await response.json();
+    setNews(data);
+    console.log(data);
+  };
 
   useEffect( () => {
-    const getId = async () => {
-      const response = await fetch("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
-      const ids = await response.json();
-      setStories(ids.hits);
-      console.log(ids.hits);
-    };
     getId();
+  },[]);
+
+  useEffect( () => {
+    
+    getStory(29761166);
   },[]);
 
   return (
     <div className="App">
       <Navbar />
       
-      <News />
-      <News />
-      <News />
-      <News />
-      <News />
+      {/* {[...stories(30)].map((story, i) => {
+        getStory(story);
+
+        
+        // useEffect(getStory(story),[]);
+      })}; */}
+
+      {/* { news.map(story => {
+        <News 
+        key={story.id} 
+        title={story.title} 
+        author={story.by} 
+        comments={story.kids.length} 
+        time={story.time} 
+        upvotes={story.score} 
+        />
+      })
+      } */}
+
     </div>
   );
 }
